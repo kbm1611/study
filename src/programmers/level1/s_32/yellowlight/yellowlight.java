6f31c975bfbdb63c66d1b62366adf7420426c42f
kbm1611 <bm1611@naver.com>
2026-06-26 23:07:23 +0900

    노란 신호등 문제(미완)

---
+++ src/programmers/level1/s_32/yellowlight/yellowlight.java
@@ -0,0 +1,17 @@
+package programmers.level1.s_32.yellowlight;
+
+public class yellowlight {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int result = sol.solution(new int[][]{{2,1,2}, {5,1,1}});
+        System.out.println( result );
+    }
+}
+
+class Solution {
+    public int solution(int[][] signals) {
+        int answer = 0;
+        return answer;
+    }
+}
