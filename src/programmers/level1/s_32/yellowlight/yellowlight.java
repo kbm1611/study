5aeb9e762bb811c0b5c999449225f292f4439a4d
kbm1611 <bm1611@naver.com>
2026-06-27 23:20:25 +0900

    노란 신호등 문제(미완)

--- src/programmers/level1/s_32/yellowlight/yellowlight.java
+++ src/programmers/level1/s_32/yellowlight/yellowlight.java
@@ -1,5 +1,7 @@
 package programmers.level1.s_32.yellowlight;
 
+import java.util.Arrays;
+
 public class yellowlight {
 
     public static void main(String[] args) {
@@ -21,10 +23,75 @@ class Solution {
     public int solution(int[][] signals) {
         int answer = 0;
 
-        // 노란불 지속시간 + 그 외의 지속시간
-        int[] yellow = new int[signals.length];
-        int[] etc = new int[signals.length];
+        int signalNum = signals.length;
+
+        int[] startPoint = new int[signalNum]; // 시작 지점
+        int[] yellow = new int[signalNum]; // 지속시간
+        int[] sum = new int[signalNum]; // 지속시간 전체 합
+
+        // 각 배열 지속 시간 넣기
+        for( int i = 0; i < signalNum; i++ ){
+            startPoint[i] = signals[i][0] + 1; // 시작 지점
+            yellow[i] = signals[i][1]; // 노란불 길이 1
+            sum[i] = signals[i][0] + signals[i][1] + signals[i][2]; // 초록불 + 노란불 + 빨간불
+        }
+
+        System.out.println("시작 지점");
+        System.out.println( Arrays.toString( startPoint ) );
+        System.out.println("\n노란불 지속시간");
+        System.out.println( Arrays.toString( yellow ) );
+        System.out.println("\n전체 지속시간 합");
+        System.out.println( Arrays.toString( sum ) );
+
+        // 시작지점 부터 노란불 지속 시간 - 그 외 지속시간 - 노란불 - 그 외 반복
+        // -1를 반환하는 기준: 신호등 sum 배열 중 같은 배열이 존재하고 그 두 배열이 서로 첫번째 루프때 겹치지 않는다면.
+        // 최소공배수를 찾는 로직을 도입하여 계산.
+        int gcd = getGCD( sum );
+
+        // 서로소 인 경우(1)
+        if( gcd == 1 ){
+            for (int num : sum) {
+                gcd *= num;
+            }
+        }
+
+        int startPos = 0; // 시작위치
+
+        // 시작위치가 gcd(최소공배수)를 넘길 경우 -1 return
+        while( startPos < gcd ){
+
+        }
+
 
         return answer;
     }
+
+    // 최대 공약수 구하는 함수
+    public int GCD(int num1, int num2){
+        if( num2 == 0 ) return num1;
+        return GCD(num2, num1 % num2);
+    }
+
+    // 최소 공약수 구하는 함수
+    public int LCM(int num1, int num2){
+        return num1 * num2 / GCD( num1, num2 );
+    }
+
+    public int getGCD(int[] numbers){
+        if(numbers == null || numbers.length == 0) return 0;
+
+        // 배열의 첫 번째 값으로 시작
+        int first = numbers[0];
+
+        // 두번째 값부터 순차적으로 누적하여 GCD 계산
+        for( int i = 1; i < numbers.length; i++ ){
+            first = GCD( first, numbers[i] );
+
+            // 중간에 최대공약수가 1이 되면 더 이상 계산할 필요없이 서로소임.
+            if( first == 1 ) return 1;
+        }
+
+        return first;
+    }
+
 }
