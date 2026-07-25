935f480ee025aa431a2f4068bb5f2fb9eb0e661f
bm1611 <bm1611@naver.com>
2026-07-25 22:17:44 +0900

    h_index 첫 커밋

---
+++ src/level2/H_Index.java
@@ -0,0 +1,17 @@
+package level2;
+
+public class H_Index {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int result = sol.solution(new int[]{3,0,6,1,5});
+        System.out.println( "result = " + result );
+    }
+}
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        return answer;
+    }
+}
